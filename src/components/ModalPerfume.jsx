c1fa584a00b7b75483c922979ed56f9ec935dc8a
JohanBoDev <127339175+JohanBoDev@users.noreply.github.com>
2025-04-07 15:57:50 -0500

    ss

--- src/components/ModalPerfume.jsx
+++ src/components/ModalPerfume.jsx
@@ -1,5 +1,7 @@
+
 const ModalPerfume = ({ perfume, onClose }) => {
   if (!perfume) return null;
+  
 
   return (
     <div className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm flex items-center justify-center p-4">
